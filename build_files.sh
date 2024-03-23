06761fac1003e96799cbe85b311299f6cc31459f
ShriRamaMoorthy <rams671011@gmail.com>
2024-03-24 01:06:21 +0530

    pip freeze added

--- build_files.sh
+++ build_files.sh
@@ -3,3 +3,9 @@ echo "Build"
 pip3 install -r requirements.txt
 pip3 install opencv-python
 pip3 install opencv-contrib-python
+
+echo "Python Version:"
+python3.10 --version
+
+echo "Installed Packages:"
+pip3 freeze
