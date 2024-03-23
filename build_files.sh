b3f9bae92820cea6bc7c3155fb7402986bcaa71d
ShriRamaMoorthy <rams671011@gmail.com>
2024-03-24 00:44:08 +0530

    python

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 alias pip=pip3
-pip --version
-pip3 --version
-python --version
 echo "Build"
-pip install -r requirements.txt
-pip install opencv-python
-pip install opencv-contrib-python
+pip3 install -r requirements.txt
+pip3 install opencv-python
+pip3 install opencv-contrib-python
