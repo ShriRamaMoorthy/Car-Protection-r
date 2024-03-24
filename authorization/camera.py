b3898e0ea17004637a1756274a15a65640386127
ShriRamaMoorthy <rams671011@gmail.com>
2024-03-24 18:47:31 +0530

    camera.py updated

--- authorization/camera.py
+++ authorization/camera.py
@@ -18,7 +18,13 @@ class VideoCamera(object):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.video = cv2.VideoCapture(0)
+        for i in range(4):  # Try up to index 3
+            video = cv2.VideoCapture(i)
+            if video.isOpened():
+                print(f"Successfully opened camera with index {i}")
+                break
+            else:
+                print(f"Failed to open camera with index {i}")
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
